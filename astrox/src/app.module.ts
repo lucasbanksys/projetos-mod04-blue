4b15eb9fe69e759f9b25f6b5ec5e8a17b6d39583
lucasbanksys <lucs@live.jp>
2021-11-09 22:19:26 -0300

    auth init

--- astrox/src/app.module.ts
+++ astrox/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, MoviesModule],
+  imports: [UsersModule, MoviesModule, AuthModule],
 })
 export class AppModule {}
